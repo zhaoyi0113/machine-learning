aeeb34e0580b4e5f44e268e3eae257700d71df27
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-08 22:00:08 +1000

    add simple linear regression

---
+++ ml-r/SimpleLinearRegression.R
@@ -0,0 +1,14 @@
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+
+# Apply the lm() function.
+relation <- lm(y~x)
+print(summary(relation))
+
+# Find weight of a person with height 170.
+a <- data.frame(x = 170)
+result <-  predict(relation,a)
+print(result)
+
+plot(y,x,col = "blue",main = "Height & Weight Regression",
+     abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
