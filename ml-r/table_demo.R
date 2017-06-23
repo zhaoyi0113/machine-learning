e50ea7b52b0d106b7ba7e3f7cdea93c4ad4f70dd
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-23 16:24:06 +1000

    add table demo

---
+++ ml-r/table_demo.R
@@ -0,0 +1,43 @@
+
+flights <- fread("data/flights14.csv")
+
+# Get all the flights with “JFK” as the origin airport in the month of June.
+flights[origin == "JFK" & month == 6L]
+
+# Get the first two rows from flights.
+flights[1:2]
+
+# Sort flights first by column origin in ascending order, and then by dest in descending order
+flights[order(origin, -dest)]
+
+# Select arr_delay column, but return it as a vector.
+flights[, arr_delay]
+
+# Select arr_delay column, but return as a data.table instead.
+flights[, list(arr_delay)]
+
+# Select both arr_delay and dep_delay columns.
+flights[, .(arr_delay, dep_delay)]
+
+# Select both arr_delay and dep_delay columns and rename them to delay_arr and delay_dep.
+flights[, .(delay_arr = arr_delay, delay_dep = dep_delay)]
+
+# How many trips have had total delay < 0?
+flights[, sum((arr_delay + dep_delay) < 0)]
+
+# Calculate the average arrival and departure delay for all flights with “JFK” as the origin airport in the month of June.
+flights[origin == "JFK" & month == 6L, .(m_arr = mean(arr_delay), m_dep = mean(dep_delay))]
+
+# – How many trips have been made in 2014 from “JFK” airport in the month of June?
+flights[origin == "JFK" & month == 6L, length(dest)]
+
+#.N is a special in-built variable that holds the number of observations in the current group. 
+#It is particularly useful when combined with by as we’ll see in the next section. 
+#In the absence of group by operations, it simply returns the number of rows in the subset.
+#So we can now accomplish the same task by using .N as follows:
+flights[origin == "JFK" & month == 6L, .N]
+
+# Select both arr_delay and dep_delay columns the data.frame way.
+flights[, c("arr_delay", "dep_delay"), with = FALSE]
+
+
