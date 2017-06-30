93015c836cbdb5442e17bdf2073b86294358cea9
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-30 20:49:24 +1000

    update house price

--- kaggle/house-price/house-price.R
+++ kaggle/house-price/house-price.R
@@ -16,6 +16,7 @@ library(RColorBrewer)
 library(dplyr)
 library(e1071)    
 library(corrplot)
+library(gridExtra)
 
 #load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr')
 #install.lib <- load.libraries[!load.libraries %in% installed.packages()]
